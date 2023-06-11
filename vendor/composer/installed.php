fb8b40b263222a55cd5a31a4af532f73b9cfe43d
zeyuanlucky <127177116+zeyuanlucky@users.noreply.github.com>
2023-06-11 10:36:06 +0800

    11

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '211e8c2394895b8eea1a3b595acc8fe11c1a05bb',
+        'name' => 'plant/aloe',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'plant/aloe' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '211e8c2394895b8eea1a3b595acc8fe11c1a05bb',
+            'dev_requirement' => false,
+        ),
+    ),
+);
